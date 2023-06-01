edd57d80da5163ea1e623b15fe21e3f3212a6669
Kumar Raj Ranjan <Kumar.Raj@trivium-esolutions.com>
2023-06-01 18:02:39 +0530

    login api implementation

---
+++ AdminSiteApi/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using AdminSiteApi.Model;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace AdminSiteApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LoginController : Controller
+    {
+
+        public readonly IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        private SqlConnection con;
+        //To Handle connection related activities    
+        private void connection()
+        {
+            string constr = _configuration.GetConnectionString("DefaultConnection");
+            con = new SqlConnection(constr);
+
+        }
+
+        [HttpPost]
+        public IActionResult Validate(Login login)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    connection();
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("select * from Login where username=@un and password=@pwd", con);
+                    cmd.Parameters.AddWithValue("@un", login.UserName);
+                    cmd.Parameters.AddWithValue("@pwd", login.Password);
+                    SqlDataReader dr = cmd.ExecuteReader();
+
+                    if (dr.Read())
+                    {
+                        return Ok("Login Successfully");
+                    }
+                }
+                finally { con.Close(); }
+            }
+            return BadRequest("invalid credential");
+        }
+
+    }
+}
---
+++ AdminSiteApi/Model/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace AdminSiteApi.Model
+{
+    public class Login
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+    }
+}
